6c52e1768d9ddb1d59f7b96086851d26b0467096
KaleidonKep99 <kaleidonkep99@outlook.com>
2022-02-28 09:12:20 +0100

    Update Program.cs

--- OSCTest/Program.cs
+++ OSCTest/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Net;
 using System.Net.Sockets;
+using System.Text;
 using Bespoke.Osc;
 
 namespace Test01
@@ -9,64 +10,110 @@ namespace Test01
         static readonly IPAddress IP = IPAddress.Loopback;
         static readonly int Port = 9000;
         static readonly IPEndPoint VRChat = new IPEndPoint(IP, Port);
+        static Random Random = new Random();
 
         static void Main(string[] args)
         {
             // Create a bogus port for the client
             OscPacket.UdpClient = new UdpClient(10025);
 
-            int A = 0;
-
             while (true)
             {
                 try
                 {
-                    if (A > 7) A = 0;
+                    // Build the message(s) by providing your parameter target(s) and its (or their) value(s)
+                    VRChatMessage Msg1 = new VRChatMessage("GestureLeftWeight", (float)Random.NextDouble());
+                    VRChatMessage Msg2 = new VRChatMessage("GestureRightWeight", (float)Random.NextDouble());
 
-                    // Send the packet with the following values
-                    SendPacket("GestureLeft", A);
+                    // Send the packet(s) using the SendPacket function
+                    SendPacket(Msg1, Msg2);
 
-                    Console.Clear();
-                    Console.WriteLine(String.Format("Sent {0} to GestureLeft!", A));
+                    LogToConsole("Sent to GestureLeftWeight and GestureRightWeight!", Msg1, Msg2);
 
                     Thread.Sleep(600);
-
-                    A++;
                 }
-                catch { 
-                    Console.Clear();
-                    Console.WriteLine("Error!"); 
+                catch {
+                    LogToConsole("Error!"); 
                 }
             }
         }
 
-        static void SendPacket(string Target, object Param)
+        static void SendPacket(params VRChatMessage[] Params)
         {
-            try
+            foreach (var Param in Params)
             {
-                // Check if Param is of one of the following supported types
-                if (Param.GetType() != typeof(int) &&
-                    Param.GetType() != typeof(float) &&
-                    Param.GetType() != typeof(bool))
-                    throw new Exception(String.Format("Param of type {0} is not supported by VRChat!", Param.GetType().ToString()));
+                try
+                {
+                    // Check if there's a valid target
+                    if (Param.Parameter == null || string.IsNullOrEmpty(Param.Parameter))
+                        throw new Exception("Parameter target not set!");
 
-                // Create a bundle that contains the target address and port (VRChat works on localhost:9000)
-                OscBundle VRBundle = new OscBundle(VRChat);
+                    // Check if Parameter is not null and of one of the following supported types
+                    if (Param.Data != null &&
+                        Param.Data.GetType() != typeof(int) &&
+                        Param.Data.GetType() != typeof(float) &&
+                        Param.Data.GetType() != typeof(bool))
+                        throw new Exception(String.Format("Param of type {0} is not supported by VRChat!", Param.Data.GetType()));
 
-                // Create the message, and append the parameter to it
-                OscMessage Message = new OscMessage(VRChat, String.Format("/avatar/parameters/{0}", Target));
-                Message.Append(Param);
+                    // Create a bundle that contains the target address and port (VRChat works on localhost:9000)
+                    OscBundle VRBundle = new OscBundle(VRChat);
 
-                // Append the message to the bundle
-                VRBundle.Append(Message);
+                    // Create the message, and append the parameter to it
+                    OscMessage Message = new OscMessage(VRChat, String.Format("/avatar/parameters/{0}", Param.Parameter));
+                    Message.Append(Param.Data);
 
-                // Send the bundle to the target address and port
-                VRBundle.Send(VRChat);
+                    // Append the message to the bundle
+                    VRBundle.Append(Message);
+
+                    // Send the bundle to the target address and port
+                    VRBundle.Send(VRChat);
+
+                }
+                catch (Exception ex)
+                {
+                    LogToConsole(ex.ToString());
+                }
             }
-            catch
+        }
+
+        static void LogToConsole(string Message, params VRChatMessage[] Parameters)
+        {
+            StringBuilder MessageBuilder = new StringBuilder();
+
+            MessageBuilder.Append(String.Format("{0} - {1}", DateTime.UtcNow.ToString("dd/MM/yyyy HH:mm:ss.ffff"), Message));
+
+            if (Parameters.Length > 0)
             {
-                Console.WriteLine("Something went wrong! Maybe the Param was of an unsupported type?");
+                MessageBuilder.Append(" (");
+
+                var LastParam = Parameters[Parameters.Length - 1];
+                foreach (var Parameter in Parameters)
+                {
+                    MessageBuilder.Append(String.Format("{0} of type {1}", Parameter.Data, Parameter.Data.GetType()));
+
+                    if (Parameter != LastParam)
+                        MessageBuilder.Append(", ");
+                }
+
+                MessageBuilder.Append(")");
             }
+
+            Console.WriteLine(MessageBuilder.ToString());
+        }
+    }
+
+    public class VRChatMessage
+    {
+        // The target of the data
+        public string? Parameter { get; }
+
+        // The data itself
+        public object? Data { get; }
+
+        public VRChatMessage(string A, object B)
+        {
+            Parameter = A;
+            Data = B;
         }
     }
 }
